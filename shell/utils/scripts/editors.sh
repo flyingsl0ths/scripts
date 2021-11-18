29afd672f306f150253bb49547d10e26a501fb90
flyingsl0ths <randomemail32110@gmail.com>
2021-11-18 10:14:58 -0600

    Add ability to switch themes

--- shell/utils/scripts/editors.sh
+++ shell/utils/scripts/editors.sh
@@ -2,6 +2,15 @@
 
 EDITORS=("VSCode" "DrRacket" "Intellij" "Android-Studio" "Typora")
 
+DMENU_FONT="JetBrainsMonoMedium Nerd Font Mono:size=13:antialias=true"
+DMENU_PROMPT="Editor"
+
+# themes
+DRACULA="-nb #44475a -nf #f8f8f2 -sb #bd93f9 -sf #f8f8f2"
+NORD="-nb #2E3440 -nf #D8DEE9 -sb #5E81AC -sf #2E3440"
+
+DMENU_THEME=$NORD
+
 launch_editor() {
     selection="$1"
 
@@ -26,7 +35,7 @@ launch_editor() {
 
 launch_editor_selector() {
     selected=$(echo "${EDITORS[*]}" | xargs printf %s'\n' |
-        dmenu -l 3 -p 'Editor' -m 0 -fn "JetBrainsMonoMedium Nerd Font Mono:size=13" -nb '#44475a' -nf '#f8f8f2' -sb '#bd93f9' -sf '#f8f8f2' -i)
+        dmenu -l 3 -p "$DMENU_PROMPT" -m 0 -fn "$DMENU_FONT" $DMENU_THEME  -i)
 
     launch_editor "$selected"
 }
