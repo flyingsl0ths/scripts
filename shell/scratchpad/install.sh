5a3d1878b6a1c4110503000c07a580aa29735722
flyingsl0ths <randomemail32110@gmail.com>
2021-11-28 22:29:37 -0600

    Update scratchpad install script

--- shell/scratchpad/install.sh
+++ shell/scratchpad/install.sh
@@ -2,11 +2,23 @@
 
 SCRIPT_NAME="scratchpad"
 
-TEMPLATES_DIR="templates"
+TEMPLATES_DIR="Templates"
 
 BIN_DIRECTORY=".local/bin"
 
 INSTALL_DIRECTORY="$HOME/$BIN_DIRECTORY"
+
+echo "Locating $INSTALL_DIRECTORY in $HOME..."
+
+[ -d "$HOME/$INSTALL_DIRECTORY" ] || (echo "$HOME/$INSTALL_DIRECTORY not found exiting..." && exit)
+
+echo "Locating $HOME/$BIN_DIRECTORY..."
+
+[ -d "$HOME/$BIN_DIRECTORY" ] || (echo "$HOME/$BIN_DIRECTORY not found exiting..." && exit)
+
+echo "Found $BIN_DIRECTORY/"
+
+echo "Installing script and templates"
 
 install_script() {
     cp "$SCRIPT_NAME.sh" "$INSTALL_DIRECTORY/$SCRIPT_NAME"
@@ -16,13 +28,6 @@ install_script() {
     ln -s "$(pwd)/$TEMPLATES_DIR" "$HOME"
 }
 
-echo "Locating $HOME/$BIN_DIRECTORY..."
-
-[ -d "$HOME/$BIN_DIRECTORY" ] || (echo "$HOME/$BIN_DIRECTORY not found exiting..." && exit)
-
-echo "Found $BIN_DIRECTORY/"
-
-echo "Installing script and templates directory"
 
 install_script
 
